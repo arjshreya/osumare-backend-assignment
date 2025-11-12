cac4c77c1557953bf50e30f6d29910e39b5d0da6
shreya arjugade <arjshreya1625@gmail.com>
2025-11-12 14:26:45 +0530

    Added PUT/tasks/:id to update existing tasks and DELETE/tasks/:id to remove tasks by id

--- Server.js
+++ Server.js
@@ -37,7 +37,30 @@ app.post('/tasks',(req,res)=>{
     tasks.push(newTask);
     res.status(201).json(newTask);
 });
+//PUT-update a task
+app.put('/tasks/:id',(req,res)=>{
+    const task = tasks.find(t=>t.id === parseInt(req.params.id));
+    if(!task){
+        return res.status(404).json({message:'Task not found'});
+    }
+    const {title,description}= req.body;
+    if(!title||!description){
+        return res.status(400).json({message:'Title and description required'});
+    }
+    task.title=title;
+    task.description=description;
+    res.json(task);
+});
 
+app.delete('/tasks/:id',(req,res)=>{
+    const taskIndex =tasks.findIndex(t=>t.id ===parseInt(req.params.id));
+    if(taskIndex ===-1)
+        {
+            return res.status(404).json({message:'Task not found'});
+        }
+    tasks.splice(taskIndex,1);
+    res.json({message:'Task deleted succesfully'});
+});
 
 app.listen(PORT, () =>{
     console.log(`Server is running on port ${PORT}`);
