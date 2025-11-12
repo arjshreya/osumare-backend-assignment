17b2772554ab781e2c5dd72ba67c3e1c01f2e5c4
shreya arjugade <arjshreya1625@gmail.com>
2025-11-12 15:30:27 +0530

    Added pagination,filtering and sorting to GET/tasks endpoint

--- Server.js
+++ Server.js
@@ -5,9 +5,49 @@ const PORT = 5000;
 app.use(express.json());
 
 let tasks=[];
-
+//GET-to fetch all tasks from list
 app.get('/tasks',(req,res)=>{
-    res.json(tasks);
+    let page= parseInt(req.query.page)||1;
+    let limit= parseInt(req.query.limit)||10;
+    let{sort, order, title, description}=req.query;
+    order=order ==='desc'?'desc':'asc';
+
+
+    //filtering
+    let filteredTasks =tasks.filter(t=>{
+        let MatchTitle=true;
+        let MatchDescription=true;
+    if(title){
+        MatchTitle = t.title.toLowerCase().includes(title.toLowerCase());
+    
+    }
+    if(description){
+        MatchDescription = t.description.toLowerCase().includes(description.toLowerCase());
+
+    }
+    return MatchTitle && MatchDescription;
+});
+    //sorting
+    if(sort){
+        filteredTasks.sort((a,b)=>{
+            if(a[sort] > b[sort])return order==='asc'?1:-1;
+            if(a[sort] < b[sort])return order==='asc'?-1:1;
+            return 0;
+        });
+    }
+    //pagination
+    let startIndex=(page - 1)*limit;
+    let endIndex= startIndex + limit;
+    const paginateTask=filteredTasks.slice(startIndex,endIndex);
+    
+    const response={
+        page: page,
+        limit: limit,
+        totalTasks: filteredTasks.length,
+        totalpages: Math.ceil(filteredTasks.length/limit),
+        tasks: paginateTask
+    };
+    res.json(response);
 });
 
 app.get('/tasks/:id',(req,res)=>{
@@ -22,7 +62,7 @@ app.get('/',(req,res)=>{
     res.send("Server ruuning properly!");
 });
 
-//POST-to add new task
+//POST-to add a new task in list
 app.post('/tasks',(req,res)=>{
     const{title,description}=req.body;
     if(!title || !description){
@@ -37,7 +77,7 @@ app.post('/tasks',(req,res)=>{
     tasks.push(newTask);
     res.status(201).json(newTask);
 });
-//PUT-update a task
+//PUT-update a task by id in a list
 app.put('/tasks/:id',(req,res)=>{
     const task = tasks.find(t=>t.id === parseInt(req.params.id));
     if(!task){
